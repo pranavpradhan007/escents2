44b2baa21f078a0d86d66666a0a3aefaa95e7e1d
pranavpradhan007 <pranavpradhan00721@gmail.com>
2023-05-14 16:20:27 +0530

    msg

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -5,25 +5,22 @@ import Loading from "./Loading";
 
 export class Item extends Component {
   render() {
-    let {title , description, imageurl , id}=this.props;
+    let { title, description, imageurl, id } = this.props;
     return (
       <div className="my-3">
-        <div className="card" style={{width: "18rem"}}>
-          <Link className="" to="/itemdetails/">
+        <div className="card" style={{ width: "18rem" }}>
           <img src={imageurl} className="card-img-top" alt="..." />
-          </Link>
+
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
-            <p className="card-text">
-              {description}
-            </p>
-            <div className="btn btn-sm btn-secondary"
-            onClick={()=>(window.location.href=`/itemdetails/${id}`)}
+            <p className="card-text">{description}</p>
+            <div
+              className="btn btn-sm btn-secondary"
+              onClick={() => (window.location.href = `/itemdetails/${id}`)}
             >
               Buy Now
             </div>
           </div>
-          
         </div>
       </div>
     );
